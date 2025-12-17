0b38b565f8711c98f8c9dcb3c46e82fc2b596a3c
Ryan Sweeney <rdsweeney@sep.com>
2025-12-17 16:29:44 -0500

    day8 complete

--- adventTS/src/days/day8.ts
+++ adventTS/src/days/day8.ts
@@ -1,4 +1,14 @@
 function findUniqueToy(toy: string): string {
-  // Code here
-  return ''
+  const lowerToy = toy.toLowerCase();
+  const characterCounts: { [key: string]: number } = {};
+  for (const char of lowerToy) {
+    if (characterCounts[char]) characterCounts[char]++;
+    else characterCounts[char] = 1;
+  }
+  for(let i=0; i < toy.length; i++) {
+    if(characterCounts[lowerToy[i]] === 1) return toy[i];
+  }
+  return "";
 }
+
+export const result = findUniqueToy("aAaAaAF");
--- adventTS/src/main.ts
+++ adventTS/src/main.ts
@@ -1,4 +1,4 @@
-import { result } from './days/day9'
+import { result } from './days/day8'
 import './style.css'
 
 console.info(result)
